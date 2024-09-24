4dea87fc29ce007e2754ce8da75f8189c2a9ced3
Red Panda <Deep_Sea_Penguin@protonmail.com>
2024-09-24 12:01:20 -0400

    init commit

---
+++ scripts/script.js
@@ -0,0 +1,13 @@
+let colorArray = []
+// Cache at least one element using getElementById
+let inputForm = document.getElementById('input-form')
+// Cache at least one element using querySelector or querySelectorAll
+let colorContainer = document.querySelector('#color-container')
+inputForm.addEventListener('submit', function(e){
+    e.preventDefault();
+    let newColorObj = {}
+    newColorObj.firstNum = document.querySelector('#num1').value
+    newColorObj.secondNum = document.querySelector('#num2').value
+    newColorObj.thirdNumb = document.querySelector('#num3').value
+    console.log(newColorObj)
+})
